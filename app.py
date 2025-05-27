69752f52d70e124bb85e5299d82e67d432b836ba
saranya <saranyapothina07@gmail.com>
2025-05-27 17:14:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from ui.video_capture import get_lip_frames
 from model.predict import load_model, predict_lip_sequence
 
-st.title("🧠 SilentSpeech AI")
+st.title("SilentSpeech AI")
 st.write("Real-time lip reading AI that converts silent video to text.")
 
 if st.button("Start Live Lip Reading"):
